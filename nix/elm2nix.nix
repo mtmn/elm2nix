fadcde8550a7d9137f0cc2d0db756fd0e2b9645e
Dwayne Crooks <me@dwaynecrooks.com>
2026-01-30 06:32:09 -0400

    Be very specific about the files needed to build the elm2nix derivation

--- nix/elm2nix.nix
+++ nix/elm2nix.nix
@@ -1,6 +1,20 @@
-{ haskellPackages }:
+{ haskellPackages, lib }:
 
+let
+  fs = lib.fileset;
+in
 (haskellPackages.callPackage ./generated/elm2nix.nix {}).overrideAttrs (old: {
+  src = fs.toSource {
+    root = ../.;
+    fileset = fs.unions [
+      ../app
+      ../src
+      ../test
+      ../elm2nix.cabal
+      ../LICENSE
+    ];
+  };
+
   #
   # N.B. Even though `doCheck = false` by default I add this `preCheck`
   #      to ensure that downstream users don't have to worry about it.
